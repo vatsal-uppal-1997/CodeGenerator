9bb7b84c095cf71e75d04b9c7b4526ca7961c8a5
Savar Kaul <42603826+savarkaul@users.noreply.github.com>
2019-06-03 12:09:30 +0530

    Update ButtonPanel.java

--- src/gui/ButtonPanel.java
+++ src/gui/ButtonPanel.java
@@ -1,8 +1,20 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -29,15 +41,15 @@ public class ButtonPanel extends JPanel {
 
 		// panel size
 		Dimension dim = getPreferredSize();
-		dim.width = 250;
+		dim.width = 300;
 		setPreferredSize(dim);
 
 		// labels
-		txtLabel = new JLabel("Create new text file: ");
-		angularLabel = new JLabel("New Angular Project: ");
-		javaLabel = new JLabel("New Java Application: ");
-		dbLabel = new JLabel("Create new collection : ");
-		testLabel = new JLabel("Generate Testcases: ");
+		txtLabel = new JLabel("Create a new text file : ");
+		angularLabel = new JLabel("New angular project : ");
+		javaLabel = new JLabel("New java application : ");
+		dbLabel = new JLabel("New database table : ");
+		testLabel = new JLabel("Create new testcases : ");
 
 		// buttons
 		txtBtn = new JButton("Text");
@@ -62,70 +74,111 @@ public class ButtonPanel extends JPanel {
 		// relative to the other cell
 		gc.weightx = 1;
 		gc.weighty = 0.5;
-
 		// x is horizontal & y is vertical i.e. as you increase x you'll go from
 		// left to right & as you increase y you'll go from top to bottom
 		gc.gridx = 0;
 		gc.gridy = 0;
-
 		gc.fill = GridBagConstraints.NONE;
-
 		// specifies the side of the cell the control sticks to
 		gc.anchor = GridBagConstraints.LINE_END;
 		add(txtLabel, gc);
+		txtLabel.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 12));
+		//button
 		gc.gridx = 1;
 		gc.gridy = 0;
 		gc.anchor = GridBagConstraints.LINE_START;
 		add(txtBtn, gc);
+		txtBtn.setForeground(new Color(255, 255, 255));
+		txtBtn.setBackground(new Color(255, 102, 0));
+		txtBtn.setFont(new Font("SansSerif", Font.BOLD, 12));
+		txtBtn.setMnemonic(KeyEvent.VK_T);
 
+		txtBtn.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			    Runtime rs = Runtime.getRuntime();
+			    
+			    try {
+			      rs.exec("notepad");
+			    }
+			    catch (IOException ev) {
+			      System.out.println(ev);
+			    }
+			  }
+			
+		});
+		
 		//////////////// SECOND ROW///////////////////////
+		//label
 		gc.gridy = 1;
 		gc.gridx = 0;
 		gc.weightx = 1;
 		gc.weighty = 0.5;
 		gc.anchor = GridBagConstraints.LINE_END;
 		add(angularLabel, gc);
+		angularLabel.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 12));
+		//button
 		gc.gridy = 1;
 		gc.gridx = 1;
 		gc.anchor = GridBagConstraints.LINE_START;
 		add(angularBtn, gc);
+		angularBtn.setForeground(new Color(255, 255, 255));
+		angularBtn.setBackground(new Color(255, 102, 0));
+		angularBtn.setFont(new Font("SansSerif", Font.BOLD, 12));
 
 		//////////////// THIRD ROW///////////////////////
+		//label
 		gc.gridy = 2;
 		gc.gridx = 0;
 		gc.weightx = 1;
 		gc.weighty = 0.5;
 		gc.anchor = GridBagConstraints.LINE_END;
 		add(javaLabel, gc);
+		javaLabel.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 12));
+		//button
 		gc.gridy = 2;
 		gc.gridx = 1;
 		gc.anchor = GridBagConstraints.LINE_START;
 		add(javaBtn, gc);
-
+		javaBtn.setForeground(new Color(255, 255, 255));
+		javaBtn.setBackground(new Color(255, 102, 0));
+		javaBtn.setFont(new Font("SansSerif", Font.BOLD, 12));
+		
 		//////////////// FOURTH ROW///////////////////////
+		//label
 		gc.gridy = 3;
 		gc.gridx = 0;
 		gc.weightx = 1;
 		gc.weighty = 0.5;
 		gc.anchor = GridBagConstraints.LINE_END;
 		add(dbLabel, gc);
+		dbLabel.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 12));
+		//button
 		gc.gridy = 3;
 		gc.gridx = 1;
 		gc.anchor = GridBagConstraints.LINE_START;
 		add(dbBtn, gc);
+		dbBtn.setForeground(new Color(255, 255, 255));
+		dbBtn.setBackground(new Color(255, 102, 0));
+		dbBtn.setFont(new Font("SansSerif", Font.BOLD, 12));
 
 		//////////////// FIFTH ROW///////////////////////
+		//label
 		gc.gridy = 4;
 		gc.gridx = 0;
 		gc.weightx = 1;
 		gc.weighty = 0.5;
 		gc.anchor = GridBagConstraints.LINE_END;
 		add(testLabel, gc);
+		testLabel.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 12));
+		//button
 		gc.gridy = 4;
 		gc.gridx = 1;
 		gc.anchor = GridBagConstraints.LINE_START;
 		add(testBtn, gc);
-
+		testBtn.setForeground(new Color(255, 255, 255));
+		testBtn.setBackground(new Color(255, 102, 0));
+		testBtn.setFont(new Font("SansSerif", Font.BOLD, 12));
 	}
 
 }
